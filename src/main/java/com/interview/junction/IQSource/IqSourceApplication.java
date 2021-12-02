a9a89b189f517e3c402f6c957e1b0debbc97aba9
Jay <jaykishanmehta1991@gmail.com>
2021-12-02 18:03:03 +0530

    Initial draft

---
+++ src/main/java/com/interview/junction/IQSource/IqSourceApplication.java
@@ -0,0 +1,13 @@
+package com.interview.junction.IQSource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IqSourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IqSourceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/interview/junction/IQSource/IqSourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interview.junction.IQSource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IqSourceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
